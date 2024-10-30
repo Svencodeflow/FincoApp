692a16634dc1732e5e1fd5e9b48d47bda733ca90
Svencodeflow <schlaakboot@gmail.com>
2024-10-30 11:32:23 +0100

    first

---
+++ src/main/java/de/sic/finco/fincowebapp/FincowebappApplication.java
@@ -0,0 +1,13 @@
+package de.sic.finco.fincowebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincowebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincowebappApplication.class, args);
+	}
+
+}
