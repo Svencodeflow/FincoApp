0791b63fe6c2c3f5add2bcd8726bf3a7339689ce
Svencodeflow <schlaakboot@gmail.com>
2024-11-04 12:45:39 +0100

    ,

--- src/main/java/de/sic/finco/fincowebapp/TemplateController.java
+++ src/main/java/de/sic/finco/fincowebapp/TemplateController.java
@@ -1,43 +1,60 @@
 package de.sic.finco.fincowebapp;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.time.Duration;
 
 @Controller
 public class TemplateController {
 
     @GetMapping("/")
-    public String home(Model model, HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView home(Model model, @CookieValue(value = "visitCount", defaultValue = "0") String visitCount, HttpServletResponse response) {
         model.addAttribute("username", "John Doe");
 
-        // Check if the cookie exists
-        Cookie[] cookies = request.getCookies();
-        boolean isFirstVisit = true;
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("firstVisit")) {
-                    isFirstVisit = false;
-                    break;
-                }
-            }
-        }
+        int visits = Integer.parseInt(visitCount);
+        visits++;
 
-        // Set the cookie for future visits
-        Cookie cookie = new Cookie("firstVisit", "true");
-        cookie.setMaxAge((int) Duration.ofDays(1).toSeconds()); // Cookie for one day
+        Cookie cookie = new Cookie("visitCount", Integer.toString(visits));
+        cookie.setMaxAge((int) Duration.ofDays(1).toSeconds());
         response.addCookie(cookie);
 
-        if (isFirstVisit) {
-            return "pages/landingpage";
+        if (visits == 1) {
+            return new ModelAndView("redirect:/landingpage");
+        } else if (visits == 2) {
+            return new ModelAndView("redirect:/login");
         } else {
-            return "pages/home";
+            return new ModelAndView("redirect:/home");
         }
+    }
+
+    @GetMapping("/landingpage")
+    public String landingPage() {
+        return "pages/landingpage";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "pages/login";
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(@RequestParam("username") String username, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("username", username);
+        return new ModelAndView("redirect:/home");
+    }
+
+    @GetMapping("/home")
+    public String homePage(@CookieValue(value = "username", defaultValue = "Guest") String username, Model model) {
+        model.addAttribute("username", username);
+        return "pages/home";
     }
 }
