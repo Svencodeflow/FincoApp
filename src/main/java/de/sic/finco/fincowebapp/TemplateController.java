f67653cdcffbc1998955cdd334fb65b2cc8ecda2
Svencodeflow <schlaakboot@gmail.com>
2024-11-04 13:21:42 +0100

    1

--- src/main/java/de/sic/finco/fincowebapp/TemplateController.java
+++ src/main/java/de/sic/finco/fincowebapp/TemplateController.java
@@ -6,55 +6,23 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
+
 import java.time.Duration;
 
 @Controller
 public class TemplateController {
 
     @GetMapping("/")
-    public ModelAndView home(Model model, @CookieValue(value = "visitCount", defaultValue = "0") String visitCount, HttpServletResponse response) {
+    public String home(Model model) {
         model.addAttribute("username", "John Doe");
 
-        int visits = Integer.parseInt(visitCount);
-        visits++;
-
-        Cookie cookie = new Cookie("visitCount", Integer.toString(visits));
-        cookie.setMaxAge((int) Duration.ofDays(1).toSeconds());
-        response.addCookie(cookie);
-
-        if (visits == 1) {
-            return new ModelAndView("redirect:/landingpage");
-        } else if (visits == 2) {
-            return new ModelAndView("redirect:/login");
+        if (isFirst()) {
+            return "pages/landingpage";
         } else {
-            return new ModelAndView("redirect:/home");
+            return "pages/home";
         }
     }
 
-    @GetMapping("/landingpage")
-    public String landingPage() {
-        return "pages/landingpage";
-    }
 
-    @GetMapping("/login")
-    public String loginPage() {
-        return "pages/login";
-    }
-
-    @PostMapping("/login")
-    public ModelAndView login(@RequestParam("username") String username, RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("username", username);
-        return new ModelAndView("redirect:/home");
-    }
-
-    @GetMapping("/home")
-    public String homePage(@CookieValue(value = "username", defaultValue = "Guest") String username, Model model) {
-        model.addAttribute("username", username);
-        return "pages/home";
-    }
 }
