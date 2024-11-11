84234bc7e557e38da864ee73910d85b6a51b9d17
mzakotin <mykhaylo.zakotin@web4train.de>
2024-11-11 13:52:20 +0100

    Update KategorieService.java

--- src/main/java/de/sic/finco/fincowebapp/KategorieService.java
+++ src/main/java/de/sic/finco/fincowebapp/KategorieService.java
@@ -9,25 +9,19 @@ public class KategorieService {
 
     private final KategorieRepository kategorieRepository;
 
-    public KategorieService(KategorieRepository photosRepository) {
-        this.kategorieRepository = photosRepository;
+    public KategorieService(KategorieRepository kategorieRepository) {
+        this.kategorieRepository = kategorieRepository;
     }
 
-    //public Collection<Photo> get() {
     public Iterable<Kategorie> get() {
-        //return db.values();
         return kategorieRepository.findAll();
     }
 
-    //public Photo get(String id) {
     public Kategorie get(Integer id) {
-        //return db.get(id);
         return kategorieRepository.findById(id).orElse(null);
     }
 
-    //public Photo remove(String id) {
     public void remove(Integer id) {
-        //return db.remove(id);
         kategorieRepository.deleteById(id);
     }
 
@@ -35,10 +29,7 @@ public class KategorieService {
         Kategorie kategorie = new Kategorie();
         kategorie.setName(contentType);
         kategorie.setKategorieID(Integer.parseInt(UUID.randomUUID().toString()));
-
-        //db.put(photo.getId(), photo);
         kategorieRepository.save(kategorie);
-
         return kategorie;
     }
 }
