76c1377826aa4439a8b96111acbff6bb47c65523
mzakotin <mykhaylo.zakotin@web4train.de>
2024-11-11 01:06:43 +0100

    Update Umsatz.java

--- src/main/java/de/sic/finco/fincowebapp/Umsatz.java
+++ src/main/java/de/sic/finco/fincowebapp/Umsatz.java
@@ -7,7 +7,7 @@ import org.springframework.data.relational.core.mapping.Table;
 import java.util.Date;
 
 @Table("UMSATZ")
-public class Umsatz {
+public class Umsatz extends Data{
     @Id
     @NotEmpty
     private int umsatzID;
@@ -20,13 +20,91 @@ public class Umsatz {
     @NotEmpty
     private String beschreibung;
     @NotEmpty
-    private Date datum;
+    private String kdNr;
     @NotEmpty
-    private Date datum;
+    private String userIBAN;
     @NotEmpty
-    private Date datum;
+    private Date art;
     @NotEmpty
-    private Date datum;
+    private Date kundenReferenz;
 
+    public Umsatz() {
+        super();
+    }
 
+    public int getUmsatzID() {
+        return umsatzID;
+    }
+
+    public void setUmsatzID(int umsatzID) {
+        this.umsatzID = umsatzID;
+    }
+
+    public double getBetrag() {
+        return betrag;
+    }
+
+    public void setBetrag(double betrag) {
+        this.betrag = betrag;
+    }
+
+    public Date getDatum() {
+        return datum;
+    }
+
+    public void setDatum(Date datum) {
+        this.datum = datum;
+    }
+
+    public int getKategorieID() {
+        return kategorieID;
+    }
+
+    public void setKategorieID(int kategorieID) {
+        this.kategorieID = kategorieID;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    @Override
+    String getKdNr() {
+        return kdNr;
+    }
+
+    @Override
+    void setKdNr(String kdNr) {
+        this.kdNr=kdNr;
+    }
+
+    @Override
+    String getIBAN() {
+        return userIBAN;
+    }
+
+    @Override
+    void setIBAN(String userIBAN) {
+        this.userIBAN=userIBAN;
+    }
+
+    public Date getArt() {
+        return art;
+    }
+
+    public void setArt(Date art) {
+        this.art = art;
+    }
+
+    public Date getKundenReferenz() {
+        return kundenReferenz;
+    }
+
+    public void setKundenReferenz(Date kundenReferenz) {
+        this.kundenReferenz = kundenReferenz;
+    }
 }
