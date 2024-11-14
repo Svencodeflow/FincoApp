f605cf8548a6cbac4993a46e90fa3c7b231ec6c1
mzakotin <mykhaylo.zakotin@web4train.de>
2024-11-14 11:08:46 +0100

    Update Data.java

--- src/main/java/de/sic/finco/fincowebapp/Data.java
+++ src/main/java/de/sic/finco/fincowebapp/Data.java
@@ -2,8 +2,8 @@ package de.sic.finco.fincowebapp;
 
 public interface Data {
 
-    public String getKdNr();
-    public void setKdNr(String kdNr);
-    public String getIBAN();
-    public void setIBAN(String userIBAN);
+    String getKdNr();
+    void setKdNr(String kdNr);
+    String getIBAN();
+    void setIBAN(String userIBAN);
 }
