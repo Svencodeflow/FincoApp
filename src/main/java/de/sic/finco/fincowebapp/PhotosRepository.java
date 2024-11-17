849ad3ed1f2eaff8fecc332ad7b4d53cf5210259
mzakotin <mykhaylo.zakotin@web4train.de>
2024-11-17 04:07:53 +0100

    update

--- src/main/java/de/sic/finco/fincowebapp/PhotosRepository.java
+++
@@ -1,8 +0,0 @@
-package de.sic.finco.fincowebapp;
-
-import de.smartindustrycampus.java.examples.Photo_App_Tutorial.Photo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PhotosRepository extends CrudRepository<Photo, Integer> {
-
-}
