35ba1ed156df3b14bf144f919ac1d0c8c1047466
mzakotin <mykhaylo.zakotin@web4train.de>
2024-11-19 16:08:31 +0100

    Update SecurityConfig.java

--- src/main/java/de/sic/finco/fincowebapp/SecurityConfig.java
+++ src/main/java/de/sic/finco/fincowebapp/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/", "/login", "/Landingpage", "/register", "/error", "/static/**", "/images/**", "/messages/**").permitAll()
+                        .requestMatchers("/", "dashboard", "/login", "/Landingpage", "/register", "/umsatz", "/error", "/static/**", "/images/**", "/messages/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
